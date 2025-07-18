db795d9932b54c18ca1895090d3b5cf327ff9492
Eva-Camacho <105989638+EvaCamacho09@users.noreply.github.com>
2025-07-18 08:27:41 -0500

    fix: Update hour12 option in date formatting to use 12-hour format

--- resources/js/views/Asistencias.vue
+++ resources/js/views/Asistencias.vue
@@ -630,7 +630,7 @@ export default {
         hour: "2-digit",
         minute: "2-digit",
         second: "2-digit",
-        hour12: false, // o true si prefieres "a. m./p. m."
+        hour12: true, // o true si prefieres "a. m./p. m."
       });
     };
 
